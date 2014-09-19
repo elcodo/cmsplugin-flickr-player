3222962c51982d420fec8b59bc2f66bab7de77c4
Grzegorz Bialy <grzegorz@elcodo.pl>
2014-09-19 14:00:13 +0200

    URL cleaning and formatting

--- cmsplugin_flickr_player/cms_plugins.py
+++ cmsplugin_flickr_player/cms_plugins.py
@@ -3,11 +3,13 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from cmsplugin_flickr_player.models import FlickrLink
+from .forms import FlickrLinkForm
+from .models import FlickrLink
 
 
 class FlickrPlayerPlugin(CMSPluginBase):
     model = FlickrLink
+    form = FlickrLinkForm
     name = _(u"Flickr player")
     render_template = "cms/plugins/flickr_player.html"
 
@@ -16,6 +18,9 @@ class FlickrPlayerPlugin(CMSPluginBase):
         height = instance.height or getattr(settings, "CMS_FLICKR_PLAYER_HEIGHT", "500")
 
         url = instance.url
+        if url.startswith(u"http://") or url.startswith(u"https://"):
+            url = url.replace(u"http://", u"//").replace(u"https://", u"//")
+        url = u"%s/player" % url
 
         context.update({
             'width': width,
--- cmsplugin_flickr_player/models.py
+++ cmsplugin_flickr_player/models.py
@@ -1,13 +1,29 @@
 # coding: utf-8
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
 
 
 class FlickrLink(CMSPlugin):
-    url = models.URLField(_("link"))
-    width = models.CharField(_(u"width"), max_length=5, null=True, blank=True)
-    height = models.CharField(_(u"height"), max_length=5, null=True, blank=True)
+    url = models.URLField(
+        _("link"),
+        help_text=_(u"Format: https://www.flickr.com/photos/#username#/sets/#id#"))
+    width = models.CharField(
+        _(u"width"),
+        default=getattr(settings, "CMS_FLICKR_PLAYER_WIDTH", "100%"),
+        max_length=5,
+        null=True,
+        blank=True)
+    height = models.CharField(
+        _(u"height"),
+        default=getattr(settings, "CMS_FLICKR_PLAYER_HEIGHT", "500"),
+        max_length=5,
+        null=True,
+        blank=True)
+
+    class Meta:
+        verbose_name, verbose_name_plural = _(u"flickr link"), _(u"flickr links")
 
     def __unicode__(self):
         return self.url
